e1bbc1391173650a5856dde6722160978e14a502
Eduardo Castagnari <castagnari.junior@gmail.com>
2025-01-07 13:04:07 -0300

    Eventos e criação de elemntos dinamicos

--- index.js
+++
@@ -1,3 +0,0 @@
-const inputItem = document.getElementById("input-item");
-
-console.log(inputItem.value);
---
+++ script.js
@@ -0,0 +1,30 @@
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras")
+let contador = 0;
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+        alert("Por favor, insira um item!")
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox- " + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+    
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+
+})
