efdcda6fa4bc579a6cd506a564fadfe4cf7dba4b
Eduardo Castagnari <castagnari.junior@gmail.com>
2025-01-08 12:43:14 -0300

    Exercicio aula 2: criando elemntos e manipulando o DOM com JS

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let botaoSalvar = document.getElementById("botao-salvar");
 
 botaoSalvar.addEventListener("click", () => {
-    const ul = documen.getElementById("lista-item");
-    document.createElement("li");
-    document.createElement("div");
-    document.createElement("input");
-    document.createElement("p");
+  event.preventDefault();
+  const ul = document.getElementById("lista-item");
+  const inputNomeItem = document.getElementById("input-item");
 
-    ul.appendChild()
-})
+  const itemLista = document.createElement("li");
+  const containerItem = document.createElement("div");
+  containerItem.style = "display:flex";
+  const inputItem = document.createElement("input");
+  inputItem.type = "checkbox";
+  const nomeItem = document.createElement("p");
+  nomeItem.textContent = inputNomeItem.value;
+
+  ul.appendChild(itemLista);
+  itemLista.appendChild(containerItem);
+  containerItem.appendChild(inputItem);
+  containerItem.appendChild(nomeItem);
+
+  inputNomeItem.value = "";
+});
